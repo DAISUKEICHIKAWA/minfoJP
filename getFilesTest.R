6eb8b12b08c24af266060569949bc8a944083128
Issei <Issei@CRSURX2-109n.(none)>
2011-08-03 14:44:48 +0900

    test

---
+++ getFilesTest.R
@@ -0,0 +1,9 @@
+getFiles <- function(trglist, dir=tempdir()){
+  #wget csv/xls files from MHLW to temporary directory 
+
+  tmpd <- dir #download directory
+  
+  wg <- "D:/wget-1.11.4-1-bin/bin/wget" #wget path
+
+  lapply(trglist, function(x)shell(sprintf("cd %s & %s -r %s" ,tmpd, wg, x)))
+  }
