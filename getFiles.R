a5504046d5cc668b29addbb36de1bc4baf94222b
dichika <ichikawa@hcc-jp.com>
2011-08-03 09:25:44 +0900

    first commit

---
+++ getFiles.R
@@ -0,0 +1,10 @@
+getFiles <- function(trglist){
+  #wget csv/xls files from MHLW to temporary directory 
+
+  tmpd <- tempdir() #download directory
+
+  wg <- "D:/wget-1.11.4-1-bin/bin/wget" #wget path
+
+  lapply(trglist, function(x)shell(sprintf("cd %s & %s -r %s" ,tmpd, wg, x)))
+  }
+
